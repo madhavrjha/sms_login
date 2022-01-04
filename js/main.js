8c6fed2e473ca85a9dea00c30a48eb04fb24f49f
madhavrjha-lab <madhav@nextnow.africa>
2022-01-04 20:03:41 +0530

    OTP Code Added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Switching Tab from Registration and LogIn
+
 const parentRegisterBtn = document.querySelector('#parent-register-btn');
 const parentLoginBtn = document.querySelector('#parent-login-btn');
 const parentRegisterBox = document.querySelector('#parent-register-box');
@@ -19,3 +21,102 @@ parentLoginBtn.addEventListener('click', e => {
   parentLoginBox.classList.remove('hidden');
   headerTitle.innerHTML = `<i class="fa-solid fa-child fa-2x"></i> Login as Parent`;
 });
+
+
+// Getting Another Form After Phone Number [Parent]
+
+const parentLoginForm = document.querySelector('#parent-login-form');
+const parentOTPForm = document.querySelector('#parent-otp-form');
+
+const inputBox = document.querySelector('#parent-login-form input');
+const OTPBox = document.querySelector('#parent-otp-form input');
+
+const errorBox = document.querySelector('#parent-login-form p.alert');
+const OTPErrorBox = document.querySelector('#parent-otp-form p.alert');
+
+const OTPSubmitBtn = document.querySelector('#parent-otp-form button');
+
+
+parentLoginForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // Validation Code 
+
+  let userPhoneNumber = inputBox.value;
+  console.log(userPhoneNumber);
+
+  /*
+
+    Write Phone Number
+    Validation Code Here
+    ....
+    ....
+    ....
+
+  */
+
+  let hasError = false;           
+  let errorMessage = 'This is Dynamic Error Message';
+
+  if(hasError) {
+    errorBox.innerHTML = `${errorMessage}`;
+    errorBox.classList.remove('hidden');
+
+    // Disappear the Error Message after 5 seconds
+    setTimeout(() => errorBox.classList.add('hidden'), 5000);
+
+  }
+  else {
+    // Hide Login Form
+    parentLoginForm.classList.add('hidden');
+    document.querySelector('article span').classList.add('hidden');
+
+    // Show OTP Form
+    parentOTPForm.classList.remove('hidden');
+
+    parentOTPForm.addEventListener('submit', e => {
+      e.preventDefault();
+
+      let OTPByUser = OTPBox.value;
+      console.log(OTPByUser);
+
+        /*
+
+          Write OTP
+          Validation Code Here
+          ....
+          ....
+          ....
+
+        */
+
+      let hasError = false;
+      let errorMessage = 'Invalid OTP';
+
+      if(hasError) {
+        OTPErrorBox.innerHTML = `${errorMessage}`;
+        OTPErrorBox.classList.remove('hidden');
+
+        // Disappear the Error Message after 5 seconds
+      setTimeout(() => OTPErrorBox.classList.add('hidden'), 5000);
+      }
+      else {
+        // Add Loading Icon in Submit Button
+        OTPBox.setAttribute('readonly', true);
+        OTPSubmitBtn.setAttribute('disabled', true);
+        OTPSubmitBtn.innerHTML = `<i class="fa-solid fa-spinner rotate"></i>`;
+
+        // Redirect to Home Page
+
+          /*
+
+            Write Login
+            Validation Code Here
+            ....
+            ....
+            ....
+
+          */
+      }
+    })
+  }
+})
